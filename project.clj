db90f7dfb95135b342e34e73cc128ed850dadc4f
Magnar Sveen <magnars@gmail.com>
2015-02-18 10:36:42 +0100

    Upgrade less4j and optimus dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.github.sommeri/less4j "1.8.0"]
-                 [optimus "0.15.0"]]
+                 [com.github.sommeri/less4j "1.8.5"]
+                 [optimus "0.17.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [test-with-files "0.1.0"]]
                    :plugins [[lein-midje "3.1.3"]]
