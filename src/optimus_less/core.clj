858842149e4cc18f87a372de7c3f735136e918c6
Allen Rohner <arohner@gmail.com>
2016-02-10 16:47:05 -0600

    Add less4j-javascript
    Adds support for ~`` JS syntax in less files

--- src/optimus_less/core.clj
+++ src/optimus_less/core.clj
@@ -2,12 +2,15 @@
   (:require [clojure.string :as str]
             [optimus.assets.load-css :refer [create-css-asset]]
             [optimus.assets.creation :refer [last-modified existing-resource]])
-  (:import [com.github.sommeri.less4j.core DefaultLessCompiler]))
+  (:import [com.github.sommeri.less4j.core DefaultLessCompiler ]
+           com.github.sommeri.less4j_javascript.Less4jJavascript))
 
 (defn compile-less [less]
-  (-> (DefaultLessCompiler.)
-      (.compile less)
-      (.getCss)))
+  (let [config (com.github.sommeri.less4j.LessCompiler$Configuration.)]
+    (Less4jJavascript/configure config)
+    (-> (DefaultLessCompiler.)
+      (.compile less config)
+      (.getCss))))
 
 (defn load-less-asset [public-dir path]
   (let [resource (existing-resource public-dir path)]
