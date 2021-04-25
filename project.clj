17ba8dcd1dda8068ec057c147be723d8c5fb1f89
Magnar Sveen <magnars@gmail.com>
2021-04-25 07:40:51 +0200

    Bump dependencies
    Notably not optimus. This still needs some changes to move to optimus-1.0.0-RC.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/magnars/optimus-less"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.github.sommeri/less4j "1.8.5"]
+  :dependencies [[com.github.sommeri/less4j "1.8.5"]
                  [com.github.sommeri/less4j-javascript "0.0.1"]
                  [optimus "0.17.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [test-with-files "0.1.0"]]
-                   :plugins [[lein-midje "3.1.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [midje "1.9.9"]
+                                  [test-with-files "0.1.1"]]
+                   :plugins [[lein-midje "3.2.1"]]
                    :resource-paths ["test/resources"]}})
